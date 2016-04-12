834263e99892e0353bc86a7bb07ab1455f28d263
samboy-t <samboy_t@hotmail.com>
2016-04-12 15:22:39 +1000

    Added Update List

--- client/index.js
+++ client/index.js
@@ -1,5 +1,43 @@
 var myApp = angular.module('myApp',[])
 
-myApp.controller('MyController', function($scope) {
+myApp.service('HistoryService', function($http) {
+	var baseUrl = "http://localhost:8080/"
+
+	this.saveWord = function (newWord) {
+		var url = baseUrl + "saveCurrent"
+		return $http.post(url, {"word": newWord})
+	}
+
+	this.getSaved = function () {
+		var url = baseUrl + "getSaved"
+		return $http.get(url)
+	}
+})
+
+myApp.controller('MyController', function($scope, HistoryService) {
+	$scope.words = []
 	$scope.newWord = 'cat'
+
+	$scope.saveThisWord = function () {
+		HistoryService.saveWord( $scope.newWord )
+		.then(saveSuccess, error)
+	}
+
+	$scope.getSavedWords = function() {
+		HistoryService.getSaved()
+		.then(loadSuccess, error)
+	}
+
+	function saveSuccess (json) {
+		console.log(json)
+	}
+
+	function loadSuccess (json) {
+		$scope.words = json.data
+	}
+
+	function error (err) {
+		console.log(err)
+	}
+
 })
--- server/server.js
+++ server/server.js
@@ -1,6 +1,35 @@
 var express = require('express')
 var app = express()
+var Nedb = require('nedb')
+var bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
 
 app.use(express.static('../client'))
+
+
+var database = new Nedb({ filename: './data/data.db', autoload: true })
+
+app.get('/getSaved', function (req, res) {
+	var query = {}
+
+	var done = function (err, data) {
+		console.log('I just read stuff from the database')
+		res.send(data)
+	}
+
+	database.find(query, done)
+})
+
+app.post('/saveCurrent', function (req, res) {
+	var data = { word: req.body.word, date: Date.now() }
+
+	var done = function () {
+		console.log('I just wrote to the database')
+		res.end("done")
+	}
+
+	database.insert(data, done)
+})
 
 app.listen(8080)
